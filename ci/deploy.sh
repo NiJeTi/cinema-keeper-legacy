bf0dd7cfde350d107872fd8beced8f06ecf3e957
NiJeTi <nijeti.inc@gmail.com>
2020-10-25 01:55:58 +0300

    Fixed arguments order

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,7 +7,8 @@ if [ ! -f $CONFIG_FILE ]; then
     echo "{}" > $CONFIG_FILE
 fi
 
-docker login docker.pkg.github.com \
---username $DOCKER_LOGIN --password $DOCKER_TOKEN
+docker login \
+--username $DOCKER_LOGIN --password $DOCKER_TOKEN \
+docker.pkg.github.com
 
 docker-compose up -d
