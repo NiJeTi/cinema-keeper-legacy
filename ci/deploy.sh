bb1d4fb11ff7f24d63ea85aeceeaf778c50aa475
NiJeTi <nijeti.inc@gmail.com>
2020-10-25 01:49:20 +0300

    Added Docker authentication

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,13 @@
 CONFIG_FILE=appsettings.Production.json
 
+DOCKER_LOGIN=$1
+DOCKER_TOKEN=$2
+
 if [ ! -f $CONFIG_FILE ]; then
     echo "{}" > $CONFIG_FILE
 fi
+
+docker login docker.pkg.github.com \
+--username $DOCKER_LOGIN --password $DOCKER_TOKEN
 
 docker-compose up -d
