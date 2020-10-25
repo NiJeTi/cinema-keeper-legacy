20c4774bd014c027b4728d2cd22b784d18b1d92e
NiJeTi <nijeti.inc@gmail.com>
2020-10-25 15:34:40 +0300

    Extended Discord bot configuration model

--- src/CinemaKeeper.Service/Configurations/DiscordBotConfiguration.cs
+++ src/CinemaKeeper.Service/Configurations/DiscordBotConfiguration.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 
+using Discord;
+
 namespace CinemaKeeper.Service.Configurations
 {
     [Serializable]
-    public class DiscordBotConfiguration
+    internal class DiscordBotConfiguration
     {
+        public LogSeverity LogLevel { get; set; } = LogSeverity.Info;
+        
         public string Token { get; set; } = string.Empty;
+
+        public int Permissions { get; set; } = 3152;
     }
 }
