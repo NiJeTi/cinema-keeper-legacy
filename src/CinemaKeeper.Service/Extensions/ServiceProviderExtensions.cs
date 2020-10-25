11dc6461e4a68a0c7977f3ecc311ccff91df7d1f
NiJeTi <nijeti.inc@gmail.com>
2020-10-25 12:38:00 +0300

    Service provider extension to resolve Discord bot configuration

---
+++ src/CinemaKeeper.Service/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CinemaKeeper.Service.Configurations;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CinemaKeeper.Service.Extensions
+{
+    internal static class ServiceProviderExtensions
+    {
+        public static IServiceCollection AddDiscordBotConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var discordBotConfiguration = new DiscordBotConfiguration();
+            configuration.Bind("DiscordBot",discordBotConfiguration);
+
+            return services.AddSingleton(discordBotConfiguration);
+        }
+    }
+}
