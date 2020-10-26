c6d295ca5f75738f417e0051ac42daf31ae5e947
NiJeTi <nijeti.inc@gmail.com>
2020-10-26 23:30:16 +0300

    Added prefix configuration

--- src/CinemaKeeper.Service/Configurations/DiscordBotConfiguration.cs
+++ src/CinemaKeeper.Service/Configurations/DiscordBotConfiguration.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 
-using Discord;
-
 namespace CinemaKeeper.Service.Configurations
 {
     [Serializable]
     internal class DiscordBotConfiguration
     {
-        public LogSeverity LogLevel { get; set; } = LogSeverity.Info;
-
         public string Token { get; set; } = string.Empty;
 
-        public int Permissions { get; set; } = 3152;
+        public string Prefix { get; set; } = "!";
     }
 }
