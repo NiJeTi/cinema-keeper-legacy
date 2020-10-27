64063a0619b47777336df06eda90a74f4626531e
NiJeTi <nijeti.inc@gmail.com>
2020-10-27 20:45:39 +0300

    Fixed moving of files in script

--- ci/extract_module_builds.sh
+++ ci/extract_module_builds.sh
@@ -2,5 +2,5 @@ ARTIFACTS_PATH=$1
 MODULES_PATH=$2
 MODULE_PATTERN=CinemaKeeper.Modules*.dll
 
-find $ARTIFACTS_PATH -type f -name "$MODULE_PATTERN" -exec mv {} $MODULES_PATH \;
+find $ARTIFACTS_PATH -type f -name "$MODULE_PATTERN" -exec mv -t $MODULES_PATH {} + \;
 rm -rf $ARTIFACTS_PATH
