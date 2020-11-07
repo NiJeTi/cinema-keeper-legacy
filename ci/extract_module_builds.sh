b4a8e8e9d3f16c42574801e7e32d13dc7064a5a3
NiJeTi <nijeti.inc@gmail.com>
2020-11-07 11:07:03 +0300

    Remove obsolete scripts

--- ci/extract_module_builds.sh
+++
@@ -1,6 +0,0 @@
-ARTIFACTS_PATH=$1
-MODULES_PATH=$2
-MODULE_PATTERN=CinemaKeeper.Modules*.dll
-
-find $ARTIFACTS_PATH -type f -name "$MODULE_PATTERN" -exec mv -t $MODULES_PATH {} + \;
-rm -rf $ARTIFACTS_PATH
