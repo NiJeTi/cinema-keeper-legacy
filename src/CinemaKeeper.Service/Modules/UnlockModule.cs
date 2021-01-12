8c881f5a059f48666c92d6d6ea870d9aea924bbe
Gubanov Ilya <yoyrandao@gmail.com>
2021-01-13 02:35:06 +0300

    Style fix

--- src/CinemaKeeper.Service/Modules/UnlockModule.cs
+++ src/CinemaKeeper.Service/Modules/UnlockModule.cs
@@ -17,9 +17,11 @@ namespace CinemaKeeper.Service.Modules
         public async Task Unlock()
         {
             var voiceChannel = (Context.User as SocketGuildUser)?.VoiceChannel;
+            
             if (voiceChannel == null)
             {
                 await Context.Channel.SendMessageAsync("User must be in a voice channel to use this command.");
+                
                 return;
             }
 
