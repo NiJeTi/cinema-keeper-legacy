b86bd16980228ca86da39cef18dec8368660f59e
NiJeTi <nijeti.inc@gmail.com>
2021-01-15 14:31:55 +0300

    New deployment script

--- cd/deploy_service.sh
+++ cd/deploy_service.sh
@@ -1,16 +1,35 @@
-CONFIG_FILE=appsettings.Production.json
-
+# Login to container registry
 DOCKER_LOGIN=$1
 DOCKER_TOKEN=$2
-
-if [ ! -f $CONFIG_FILE ]; then
-    echo "{}" > $CONFIG_FILE
-fi
 
 docker login \
 --username $DOCKER_LOGIN --password $DOCKER_TOKEN \
 ghcr.io
 
-docker-compose down --rmi all && \
-docker-compose pull && \
-docker-compose up -d --force-recreate
+# Create configuration file if not exists
+CONFIG_FILE=appsettings.Production.json
+
+if [ ! -f $CONFIG_FILE ]; then
+    echo "{}" > $CONFIG_FILE
+fi
+
+IMAGE_NAME=ghcr.io/nijeti/cinema-keeper
+CONTAINER_NAME=CinemaKeeper
+
+# Stop and remove existing container
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    if [ ! "$(docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
+        docker stop $CONTAINER_NAME
+    fi
+
+    docker rm $CONTAINER_NAME
+    docker rmi $IMAGE_NAME
+fi
+
+# Create new container and run it
+docker run -d \
+    --name $CONTAINER_NAME \
+    -e DOTNET_ENVIRONMENT=Production \
+    -v "$PWD/appsettings.Production.json:/app/appsettings.Production.json" \
+    -v "$PWD/Logs/:/app/Logs" \
+    $IMAGE_NAME
