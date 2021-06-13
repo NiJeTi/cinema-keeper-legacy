2c953c532a3d2e10283b2161a12bb48bc803ca86
NiJeTi <nijeti.inc@gmail.com>
2021-06-14 02:47:18 +0300

    Removed exception shield and moved logic to precondition attributes and helpers

---
+++ src/CinemaKeeper.Service/Preconditions/RequireUserInVoiceChannelAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace CinemaKeeper.Service.Preconditions
+{
+    public class UserMustBeInVoiceChannelAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command,
+            IServiceProvider services)
+        {
+            var user = (SocketGuildUser) context.User;
+
+            return Task.FromResult(user.VoiceChannel != null
+                ? PreconditionResult.FromSuccess()
+                : PreconditionResult.FromError("errors.userMustBeInVoiceChannel"));
+        }
+    }
+}
