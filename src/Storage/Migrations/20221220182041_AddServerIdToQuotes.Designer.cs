cfe05df2add732629428a5ae8b1e1513f095f51e
NiJeTi <nijeti.inc@gmail.com>
2022-12-20 21:23:04 +0300

    Add server ID to quote

--- src/Storage/Migrations/20221220182041_AddServerIdToQuotes.Designer.cs
+++ src/Storage/Migrations/20221220182041_AddServerIdToQuotes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CinemaKeeper.Storage.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CinemaKeeper.Storage.Migrations
 {
     [DbContext(typeof(Postgres))]
-    partial class PostgresModelSnapshot : ModelSnapshot
+    [Migration("20221220182041_AddServerIdToQuotes")]
+    partial class AddServerIdToQuotes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Storage/Models/Quote.cs
+++ src/Storage/Models/Quote.cs
@@ -4,17 +4,24 @@ namespace CinemaKeeper.Storage.Models
 {
     public class Quote : DatabaseEntity
     {
-        public Quote(ulong author, string text, DateTimeOffset createdAt, ulong createdBy)
+        public Quote(
+            ulong author,
+            string text,
+            DateTimeOffset createdAt,
+            ulong createdBy,
+            ulong createdOn)
         {
             Author = author;
             Text = text;
             CreatedAt = createdAt;
             CreatedBy = createdBy;
+            CreatedOn = createdOn;
         }
 
         public ulong Author { get; set; }
         public string Text { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
         public ulong CreatedBy { get; set; }
+        public ulong CreatedOn { get; set; }
     }
 }
